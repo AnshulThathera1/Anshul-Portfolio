1efaee7337ea7efc5b6f0fa7a01d61cbfa316be5
Anshul Thathera <anshulkasera35@gmail.com>
2025-07-27 12:29:00 +0530

    Name Changed

--- components/DisplayLottie.tsx
+++ components/DisplayLottie.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-import Lottie from "react-lottie";
+import React, { useState, useEffect } from "react";
 
 type Props = {
   animationPath: string;
 };
 
 const GreetingLottie = ({ animationPath }: Props) => {
+  const [LottieComponent, setLottieComponent] = useState<any>(null);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+    // Dynamically import react-lottie only on the client side
+    import("react-lottie").then((module) => {
+      setLottieComponent(() => module.default);
+    });
+  }, []);
+
+  if (!isClient || !LottieComponent) {
+    // Return a placeholder or loading state during SSR
+    return (
+      <div style={{ width: "100%", height: "300px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <div>Loading animation...</div>
+      </div>
+    );
+  }
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -14,8 +33,7 @@ const GreetingLottie = ({ animationPath }: Props) => {
 
   return (
     <div onClick={() => null}>
-      {/* @ts-ignore */}
-      <Lottie options={defaultOptions} />
+      <LottieComponent options={defaultOptions} />
     </div>
   );
 };
